95ebf3b324a8795e0fa8dac074b4f15f839f215c
MNeverkevich <mneverkevich@darwinapps.com>
2025-06-26 17:01:47 +0300

    chore: add @sentry/astro dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,26 @@ import tailwind from "@astrojs/tailwind";
 import critters from "astro-critters";
 import purgecss from "astro-purgecss";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-stest.web',
   integrations: [
     tailwind(),
     critters(),
-    purgecss('./purgecss.config.js')
+    purgecss('./purgecss.config.js'),
+    sentry({
+      dsn: "https://0dc12dce03c41a8f790b61ee42817019@o4509564935471104.ingest.de.sentry.io/4509565335175248",
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      sendDefaultPii: true,
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    })
   ],
   server: {
     port: 3000,
